13a2bbc498acfd888eaa35a45dbc82af60df1a1c
大神 <1272316324@qq.com>
2019-10-29 15:05:49 +0800

    get sission

--- app/controller/base.js
+++ app/controller/base.js
@@ -1,15 +1,17 @@
 'use strict';
 const { Controller } = require('egg');
 module.exports = class BaseConstroller extends Controller {
-  get user1() {
+  get user() {
     return this.ctx.user.sission;
   }
+
   success(data) {
     this.ctx.body = {
       code: 0,
       data,
     };
   }
+
   error(error) {
     console.error(error);
     this.ctx.body = {
